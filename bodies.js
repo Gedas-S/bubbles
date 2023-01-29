3686c0ccf88477d7b2a448dd175c9aefbe844d9a
Gedas-S <Gedas-S@users.noreply.github.com>
2023-01-29 23:54:11 +0200

    test planets

---
+++ bodies.js
@@ -0,0 +1,22 @@
+function make_planet(x, y, size, color) {
+    const planet = document.createElement("div")
+    planet.classList.add("planet")
+    planet.style.top = y + "%"
+    planet.style.left = x + "%"
+    planet.style.width = planet.style.height = size + "em"
+    planet.style.backgroundColor = color
+
+    const shadow = document.createElement("div")
+    shadow.classList.add("shadow")
+    const orbit_angle = Math.atan2(50-y, x-50)
+    const shadow_size = 4 * -Math.sin(orbit_angle) + 16
+    const gradient_x = Math.cos(orbit_angle) * -shadow_size + 50
+    const gradient_y = Math.sin(orbit_angle) * shadow_size + 50
+    shadow.style.backgroundImage = (
+        "radial-gradient(circle "+0.7*size+"em at "+gradient_x+"% "+gradient_y+"%, " +
+        "#00000000, #00000000 75%, #000000BF 85%)"
+    )
+    planet.appendChild(shadow)
+    
+    return planet
+}
---
+++ game.js
@@ -0,0 +1,8 @@
+function init() {
+    const main = document.getElementById("main")
+    main.appendChild(make_planet(30, 50, 4, "#9C2E35"))
+    main.appendChild(make_planet(70, 50, 4, "#9C2E35"))
+    main.appendChild(make_planet(50, 70, 5, "#9C2E35"))
+    main.appendChild(make_planet(50, 30, 5, "#9C2E35"))
+    main.appendChild(make_planet(20, 20, 6, "#9C2E35"))
+}
