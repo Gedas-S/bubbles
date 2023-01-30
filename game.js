07f982e430fdcb52e257545fb3f84c9584d1eee4
Gedas-S <Gedas-S@users.noreply.github.com>
2023-01-30 21:59:11 +0200

    more planet colors for testing

--- game.js
+++ game.js
@@ -2,8 +2,8 @@ function init() {
     const main = document.getElementById("main")
     main.appendChild(make_star(10, "255,255,0"))
     main.appendChild(make_planet(30, 50, 4, "#9C2E35"))
-    main.appendChild(make_planet(70, 50, 4, "#9C2E35"))
-    main.appendChild(make_planet(50, 70, 5, "#9C2E35"))
-    main.appendChild(make_planet(50, 30, 5, "#9C2E35"))
-    main.appendChild(make_planet(20, 20, 6, "#9C2E35"))
+    main.appendChild(make_planet(70, 50, 4, "rgb(255,40,60)"))
+    main.appendChild(make_planet(50, 70, 5, "rgb(50,90,255)"))
+    main.appendChild(make_planet(50, 30, 5, "rgb(120,255,0)"))
+    main.appendChild(make_planet(20, 20, 6, "rgb(120,255,190)"))
 }
