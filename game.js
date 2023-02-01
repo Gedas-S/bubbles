00ccac0702106d34b127c6d35c916bc8c983b294
Gedas-S <Gedas-S@users.noreply.github.com>
2023-02-01 20:06:48 +0200

    add game loop

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 function init() {
+    self.game = {}
     const main = document.getElementById("main")
     main.appendChild(make_star(10, "255,255,0"))
     main.appendChild(make_planet(30, 50, 4, "#9C2E35"))
@@ -6,4 +7,14 @@ function init() {
     main.appendChild(make_planet(50, 70, 5, "rgb(50,90,255)"))
     main.appendChild(make_planet(50, 30, 5, "rgb(120,255,0)"))
     main.appendChild(make_planet(20, 20, 6, "rgb(120,255,190)"))
+
+    run_main_loop()
+}
+
+function run_main_loop() {
+    game.main_loop = setInterval(main_loop, 16)
+}
+
+function main_loop() {
+
 }
